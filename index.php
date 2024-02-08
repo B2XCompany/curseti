47ad8375266957f8dbd7987382ac20a4c5ccda5d
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-02-07 22:08:07 -0300

    .

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/mainn.css">
-    <link rel="stylesheet" href="style/roott.css">
-    <link rel="stylesheet" href="style/headerr.css">
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/root.css">
+    <link rel="stylesheet" href="style/header.css">
     <link rel="shortcut icon" href="./imgs/icon.png" type="image/x-icon">
     <title>CurseTI - Cursos online gratuitos com certificado</title>
 </head>
