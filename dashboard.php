2f37b28f6b0a1ffeebaf3000e405fe8f7047cc83
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-02-07 22:09:00 -0300

    .

--- dashboard.php
+++ dashboard.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/header.css">
-    <link rel="stylesheet" href="style/root.css">
+    <link rel="stylesheet" href="style/headerr.css">
+    <link rel="stylesheet" href="style/roott.css">
     <link rel="shortcut icon" href="./icon.png" type="image/x-icon">
     <title>CurseTI - Dashboard</title>
 </head>
