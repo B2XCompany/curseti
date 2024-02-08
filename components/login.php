c8b0b3eb55783482a1a97b6c40890a5bdf93e384
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-02-07 21:56:10 -0300

    .

--- components/login.php
+++ components/login.php
@@ -5,13 +5,13 @@
         <h1 class='title-login'>Entre em sua conta</h1>
         <div class='inps-out'> 
             <input class='input-login' type='text' placeholder='exemplo@gmail.com'>
-            <input class='input-login' type='password' placeholder='******'>
+            <input class='input-login' type='password' placeholder='Senha'>
             <button class='bt-login'>Entrar</button>
         </div>
         <p class='extra-login'>Ainda não tem conta? 
             <button>Criar conta</button>
         </p>
-        <button class='extra-login'>Esqueci minha senha</button>
-        <button onclick='closeLogin()' class='back-login'>Voltar</button>
+        <button class='extra-login-bt'>Esqueci minha senha</button>
+        <button onclick='closeLogin()' class='extra-login-bt'>Voltar</button>
     </div>
 </div>
