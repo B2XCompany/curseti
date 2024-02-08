93a6edf3b4f2eccd04c2c78fb25fa91f7b8d2f4d
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-02-08 18:32:26 -0300

    .

--- components/google_log.php
+++ components/google_log.php
@@ -11,33 +11,31 @@
 <p id="mensagem"></p>
 
 <script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-  import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyDc-6F7y01xX50MuX1yr8uSdLbnwG75jYk",
-    authDomain: "curseti.firebaseapp.com",
-    projectId: "curseti",
-    storageBucket: "curseti.appspot.com",
-    messagingSenderId: "579899765617",
-    appId: "1:579899765617:web:f995e3cdc1a2cb1dbb7cd0",
-    measurementId: "G-0D45K6YH4P"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDc-6F7y01xX50MuX1yr8uSdLbnwG75jYk",
+  authDomain: "curseti.firebaseapp.com",
+  projectId: "curseti",
+  storageBucket: "curseti.appspot.com",
+  messagingSenderId: "579899765617",
+  appId: "1:579899765617:web:f995e3cdc1a2cb1dbb7cd0",
+  measurementId: "G-0D45K6YH4P"
+};
 
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth();
-    const provider = new GoogleAuthProvider();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
 
-    const logar = async() => {
-      signInWithPopup(auth, provider)
-      .then(e=>{
-        console.log(e)
-      })
-    }
-
-
+const logar = async() => {
+  signInWithPopup(auth, provider)
+  .then(e=>{
+    console.log(e)
+  })
+}
 </script>
 
 
