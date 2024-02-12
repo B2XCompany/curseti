f066cee244808e39d54df86e476e1e4a1cbc13c4
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-02-12 01:50:58 -0300

    log

--- dashboard.php
+++ dashboard.php
@@ -20,6 +20,7 @@ justLog($__EMAIL__);
             background: linear-gradient(-210deg, #202020, #101010);
             display: flex;
             justify-content: center;
+            min-height: 100svg;
         }
     </style>
 </head>
