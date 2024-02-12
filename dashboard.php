bccca50a754e112b88acb1c9113c1e4979f65e34
Bruno Wotzke <brunoricardowotzke@gmail.com>
2024-02-12 18:34:46 -0300

    .

--- dashboard.php
+++ dashboard.php
@@ -15,13 +15,18 @@ justLog($__EMAIL__);
     <title>CurseTI - Dashboard</title>
     <style>
         .left{
-            width: 250px;
+            width: 30%;
+            max-width: 250px;
             border-radius: 0 5px 5px 0;
             background: linear-gradient(-210deg, #202020, #101010);
             display: flex;
             justify-content: center;
             min-height: calc(100svh - 40px);
             padding: 20px 0;
+        }
+        .right{
+            width: 100%;
+            background: #ccc;
         }
     </style>
 </head>
